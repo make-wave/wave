e8e6b1c9654fb4171e5619d1be34fa3b6ed10f56
Gary Holland <garyhollandxyz@gmail.com>
2025-08-01 22:48:13 +0100

    begin refactor bin

--- src/bin/wave.rs
+++ src/bin/wave.rs
@@ -2,6 +2,104 @@ use clap::Parser;
 use wave::collection::{load_collection, resolve_request_vars};
 use wave::{handle_delete, handle_get, handle_patch, handle_post, handle_put, Cli};
 
+fn spinner_msg(method: &str, url: &str, params: &[String]) -> String {
+    format!(
+        "{} {}{}",
+        method,
+        url,
+        if params.is_empty() { "" } else { " " },
+    )
+}
+
+fn prepare_headers_and_body(
+    resolved: &wave::collection::Request,
+) -> (Vec<(String, String)>, String, bool) {
+    let mut headers: Vec<(String, String)> = resolved
+        .headers
+        .clone()
+        .unwrap_or_default()
+        .into_iter()
+        .collect();
+    match &resolved.body {
+        Some(wave::collection::Body::Json(map)) => {
+            fn yaml_to_json(val: &serde_yaml::Value) -> serde_json::Value {
+                match val {
+                    serde_yaml::Value::Null => serde_json::Value::Null,
+                    serde_yaml::Value::Bool(b) => serde_json::Value::Bool(*b),
+                    serde_yaml::Value::Number(n) => {
+                        if let Some(i) = n.as_i64() {
+                            serde_json::Value::Number(i.into())
+                        } else if let Some(f) = n.as_f64() {
+                            serde_json::Number::from_f64(f)
+                                .map(serde_json::Value::Number)
+                                .unwrap_or(serde_json::Value::Null)
+                        } else {
+                            serde_json::Value::Null
+                        }
+                    }
+                    serde_yaml::Value::String(s) => serde_json::Value::String(s.clone()),
+                    serde_yaml::Value::Sequence(seq) => {
+                        serde_json::Value::Array(seq.iter().map(yaml_to_json).collect())
+                    }
+                    serde_yaml::Value::Mapping(map) => {
+                        let mut obj = serde_json::Map::new();
+                        for (k, v) in map {
+                            let key = match k {
+                                serde_yaml::Value::String(s) => s.clone(),
+                                _ => serde_yaml::to_string(k).unwrap_or_default(),
+                            };
+                            obj.insert(key, yaml_to_json(v));
+                        }
+                        serde_json::Value::Object(obj)
+                    }
+                    _ => serde_json::Value::Null,
+                }
+            }
+            let json_obj = serde_json::Value::Object(
+                map.iter()
+                    .map(|(k, v)| (k.clone(), yaml_to_json(v)))
+                    .collect(),
+            );
+            if !headers
+                .iter()
+                .any(|(k, _)| k.eq_ignore_ascii_case("content-type"))
+            {
+                headers.push(("Content-Type".to_string(), "application/json".to_string()));
+            }
+            let json_str = serde_json::to_string(&json_obj).unwrap_or_else(|_| "{}".to_string());
+            (headers, json_str, false)
+        }
+        Some(wave::collection::Body::Form(map)) => {
+            let form_str =
+                wave::http_client::Client::<wave::http_client::ReqwestBackend>::prepare_form_body(
+                    &map.iter()
+                        .map(|(k, v)| (k.clone(), v.clone()))
+                        .collect::<Vec<_>>(),
+                    &mut headers.clone(),
+                );
+            if !headers
+                .iter()
+                .any(|(k, _)| k.eq_ignore_ascii_case("content-type"))
+            {
+                headers.push((
+                    "Content-Type".to_string(),
+                    "application/x-www-form-urlencoded".to_string(),
+                ));
+            }
+            (headers, form_str, true)
+        }
+        None => (headers, "".to_string(), false),
+    }
+}
+
+fn run_with_spinner_and_print<F>(spinner_msg: &str, verbose: bool, f: F)
+where
+    F: FnOnce() -> Result<wave::http_client::HttpResponse, Box<dyn std::error::Error>>,
+{
+    let result = wave::run_with_spinner(spinner_msg, f);
+    wave::printer::print_response(result, verbose);
+}
+
 fn main() {
     let cli = Cli::parse();
     use wave::Command;
@@ -11,13 +109,8 @@ fn main() {
             params,
             verbose,
         } => {
-            let spinner_msg = format!(
-                "GET {}{}{}",
-                url,
-                if params.is_empty() { "" } else { " " },
-                params.join(" ")
-            );
-            handle_get(&url, &params, verbose, &spinner_msg);
+            let msg = spinner_msg("GET", &url, &params);
+            handle_get(&url, &params, verbose, &msg);
         }
         Command::Post {
             url,
@@ -25,13 +118,8 @@ fn main() {
             form,
             verbose,
         } => {
-            let spinner_msg = format!(
-                "POST {}{}{}",
-                url,
-                if params.is_empty() { "" } else { " " },
-                params.join(" ")
-            );
-            handle_post(&url, &params, form, verbose, &spinner_msg);
+            let msg = spinner_msg("POST", &url, &params);
+            handle_post(&url, &params, form, verbose, &msg);
         }
         Command::Put {
             url,
@@ -39,13 +127,8 @@ fn main() {
             form,
             verbose,
         } => {
-            let spinner_msg = format!(
-                "PUT {}{}{}",
-                url,
-                if params.is_empty() { "" } else { " " },
-                params.join(" ")
-            );
-            handle_put(&url, &params, form, verbose, &spinner_msg);
+            let msg = spinner_msg("PUT", &url, &params);
+            handle_put(&url, &params, form, verbose, &msg);
         }
         Command::Patch {
             url,
@@ -53,26 +136,16 @@ fn main() {
             form,
             verbose,
         } => {
-            let spinner_msg = format!(
-                "PATCH {}{}{}",
-                url,
-                if params.is_empty() { "" } else { " " },
-                params.join(" ")
-            );
-            handle_patch(&url, &params, form, verbose, &spinner_msg);
+            let msg = spinner_msg("PATCH", &url, &params);
+            handle_patch(&url, &params, form, verbose, &msg);
         }
         Command::Delete {
             url,
             params,
             verbose,
         } => {
-            let spinner_msg = format!(
-                "DELETE {}{}{}",
-                url,
-                if params.is_empty() { "" } else { " " },
-                params.join(" ")
-            );
-            handle_delete(&url, &params, verbose, &spinner_msg);
+            let msg = spinner_msg("DELETE", &url, &params);
+            handle_delete(&url, &params, verbose, &msg);
         }
         Command::Collection {
             collection,
@@ -119,129 +192,45 @@ fn main() {
                                         wave::printer::print_response(result, verbose);
                                     }
                                     "POST" | "PUT" | "PATCH" => {
-                                        let mut headers: Vec<(String, String)> = resolved
-                                            .headers
-                                            .unwrap_or_default()
-                                            .into_iter()
-                                            .collect();
-                                        let (body, _is_form) = match &resolved.body {
-                                            Some(wave::collection::Body::Json(map)) => {
-                                                fn yaml_to_json(
-                                                    val: &serde_yaml::Value,
-                                                ) -> serde_json::Value
-                                                {
-                                                    match val {
-                                                        serde_yaml::Value::Null => {
-                                                            serde_json::Value::Null
-                                                        }
-                                                        serde_yaml::Value::Bool(b) => {
-                                                            serde_json::Value::Bool(*b)
-                                                        }
-                                                        serde_yaml::Value::Number(n) => {
-                                                            if let Some(i) = n.as_i64() {
-                                                                serde_json::Value::Number(i.into())
-                                                            } else if let Some(f) = n.as_f64() {
-                                                                serde_json::Number::from_f64(f)
-                                                                    .map(serde_json::Value::Number)
-                                                                    .unwrap_or(
-                                                                        serde_json::Value::Null,
-                                                                    )
-                                                            } else {
-                                                                serde_json::Value::Null
-                                                            }
-                                                        }
-                                                        serde_yaml::Value::String(s) => {
-                                                            serde_json::Value::String(s.clone())
-                                                        }
-                                                        serde_yaml::Value::Sequence(seq) => {
-                                                            serde_json::Value::Array(
-                                                                seq.iter()
-                                                                    .map(yaml_to_json)
-                                                                    .collect(),
-                                                            )
-                                                        }
-                                                        serde_yaml::Value::Mapping(map) => {
-                                                            let mut obj = serde_json::Map::new();
-                                                            for (k, v) in map {
-                                                                let key = match k {
-                                                                    serde_yaml::Value::String(
-                                                                        s,
-                                                                    ) => s.clone(),
-                                                                    _ => serde_yaml::to_string(k)
-                                                                        .unwrap_or_default(),
-                                                                };
-                                                                obj.insert(key, yaml_to_json(v));
-                                                            }
-                                                            serde_json::Value::Object(obj)
-                                                        }
-                                                        _ => serde_json::Value::Null,
-                                                    }
-                                                }
-                                                let json_obj = serde_json::Value::Object(
-                                                    map.iter()
-                                                        .map(|(k, v)| (k.clone(), yaml_to_json(v)))
-                                                        .collect(),
-                                                );
-                                                if !headers.iter().any(|(k, _)| {
-                                                    k.eq_ignore_ascii_case("content-type")
-                                                }) {
-                                                    headers.push((
-                                                        "Content-Type".to_string(),
-                                                        "application/json".to_string(),
-                                                    ));
-                                                }
-                                                let json_str = serde_json::to_string(&json_obj)
-                                                    .unwrap_or_else(|_| "{}".to_string());
-                                                (json_str, false)
-                                            }
-                                            Some(wave::collection::Body::Form(map)) => {
-                                                let form_str = wave::http_client::Client::<
-                                                    wave::http_client::ReqwestBackend,
-                                                >::prepare_form_body(
-                                                    &map.iter()
-                                                        .map(|(k, v)| (k.clone(), v.clone()))
-                                                        .collect::<Vec<_>>(),
-                                                    &mut headers.clone(),
-                                                );
-                                                if !headers.iter().any(|(k, _)| {
-                                                    k.eq_ignore_ascii_case("content-type")
-                                                }) {
-                                                    headers.push((
-                                                        "Content-Type".to_string(),
-                                                        "application/x-www-form-urlencoded"
-                                                            .to_string(),
-                                                    ));
-                                                }
-                                                (form_str, true)
-                                            }
-                                            None => ("".to_string(), false),
-                                        };
+                                        let (headers, body, _is_form) =
+                                            prepare_headers_and_body(&resolved);
                                         let rt = tokio::runtime::Runtime::new().unwrap();
-                                        let result = match method.as_str() {
-                                            "POST" => wave::run_with_spinner(&spinner_msg, || {
-                                                rt.block_on(client.post(
-                                                    &resolved.url,
-                                                    &body,
-                                                    headers,
-                                                ))
-                                            }),
-                                            "PUT" => wave::run_with_spinner(&spinner_msg, || {
-                                                rt.block_on(client.put(
-                                                    &resolved.url,
-                                                    &body,
-                                                    headers,
-                                                ))
-                                            }),
-                                            "PATCH" => wave::run_with_spinner(&spinner_msg, || {
-                                                rt.block_on(client.patch(
-                                                    &resolved.url,
-                                                    &body,
-                                                    headers,
-                                                ))
-                                            }),
+                                        match method.as_str() {
+                                            "POST" => run_with_spinner_and_print(
+                                                &spinner_msg,
+                                                verbose,
+                                                || {
+                                                    rt.block_on(client.post(
+                                                        &resolved.url,
+                                                        &body,
+                                                        headers,
+                                                    ))
+                                                },
+                                            ),
+                                            "PUT" => run_with_spinner_and_print(
+                                                &spinner_msg,
+                                                verbose,
+                                                || {
+                                                    rt.block_on(client.put(
+                                                        &resolved.url,
+                                                        &body,
+                                                        headers,
+                                                    ))
+                                                },
+                                            ),
+                                            "PATCH" => run_with_spinner_and_print(
+                                                &spinner_msg,
+                                                verbose,
+                                                || {
+                                                    rt.block_on(client.patch(
+                                                        &resolved.url,
+                                                        &body,
+                                                        headers,
+                                                    ))
+                                                },
+                                            ),
                                             _ => unreachable!(),
-                                        };
-                                        wave::printer::print_response(result, verbose);
+                                        }
                                     }
                                     _ => eprintln!("Unsupported method: {method}"),
                                 }
